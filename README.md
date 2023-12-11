5061b93d78114f07043b5bcdf09121093652304c
VornicuCatalina <100404375+VornicuCatalina@users.noreply.github.com>
2023-12-11 09:58:27 +0200

    global variables and main_menu how it would look like

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Minesweeper
 Python project
+
+# Rules for my project:
+1. The time will be chosen in seconds
---
+++ main.py
@@ -0,0 +1,18 @@
+# global variables used in the whole project
+# the game will use the default values in case that they will not be modified
+height = 10
+width = 10
+number_bombs = 10
+time_seconds = 120
+
+# window size
+"""
+window_height = 
+window_width =
+"""
+
+# click on start
+
+# click on settings
+
+# click on exit
